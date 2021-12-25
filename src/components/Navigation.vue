f1be71c542cf21ddf823e50eac4572f196f37643
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-12-25 20:08:14 -0300

    feat(Components/Navigation): componente de la barra de navegacion
    Se crea un componente para la barra de navegacion.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+   <v-app-bar app color="#434b63" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+      </div>
+
+      <v-spacer></v-spacer>
+
+			<div>
+				<v-tabs background-color="transparent" slider-color="#1fd8f2">
+						<v-tab> <router-link> Inicio </router-link></v-tab> 
+						<v-tab>Quienes somos</v-tab>
+						<v-tab>Programas</v-tab>
+						<v-tab>Como funciona</v-tab>
+				</v-tabs>
+			</div>
+    
+    </v-app-bar>
+</template>
+
+<script>
+	export default {
+		name: "Navigation"
+	};
+
+</script>
+
+<style scoped>
+
+</style>
