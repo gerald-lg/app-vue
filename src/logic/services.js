eae837d4b5c36c2272e0b759821ff037dcd7c422
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-12-26 19:05:45 -0300

    feat(services): archivo para manejar los endpoints de las API

---
+++ src/logic/services.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const ENDPOINT_PATH_LOGIN = "https://crm.redcapital.cl/api/login";
+const ENDPOINT_PATH_POSTS = "https://jsonplaceholder.typicode.com/posts";
+
+export default {
+    login(rut, password) {
+      const user = { rut, password };
+      return axios.post(ENDPOINT_PATH_LOGIN, user);
+    },
+    getPosts(userId,id, title){
+        const post = {userId, id, title};
+        return axios.get(ENDPOINT_PATH_POSTS,post);
+    }
+};
