7e093c603f8e929de0125b269a9690026eeed0f4
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-12-26 19:04:02 -0300

    feat(views): se agregan vistas principales.

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app> 
+        <div class="d-flex flex-column align-center">
+            <Form/>
+        </div>
+    </v-app>
+</template>
+
+<script>
+import Form from "../components/Form.vue"
+
+export default {
+    name: 'Login',
+    data(){
+        return {
+            rutSend :'',
+            passwordSend : '',
+        }
+    },
+    components: {
+        Form,
+    },
+    methods: {
+        
+    }
+}
+</script>
