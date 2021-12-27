51b54077cd6362ed9d7eb58235700993837fd4df
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-12-26 23:59:56 -0300

    feat(Post): interfaz inicial del componente post.

---
+++ src/components/Post.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <v-layout>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-img
+                    class="white--text"
+                    height="200px"
+                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    >
+                    </v-img>
+                    <v-card-title>
+                        <div>
+                            <span>{{title}}</span><br>
+                            <div>{{body}}</div>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn color="orange">Ver</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+
+
+    </div>
+</template>
+
+<script>
+
+export default ({
+    name: 'Post',
+    props: {
+        userId: Number,
+        id: Number,
+        title: String,
+        body: String,
+        status: Boolean,
+        amount: Number
+    } 
+    
+})
+</script>
