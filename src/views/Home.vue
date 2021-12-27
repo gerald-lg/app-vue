1d975b82b45610c566183768ecc083a18d1e8294
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-12-26 23:57:47 -0300

    feat(views/Home): se adiciona vista para usuario logueado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,39 @@
 <template>
-    <div>
-        
+    <div class="d-flex flex-column align-center my-10">
+        <!-- todo: colocar imagen de fondo -->
+        <div class="" style="text-decoration: underline #847ae1; font-size: 20px ">
+            <h1> BIENVENIDO </h1>
+        </div>
+
+        <div>
+            <v-btn
+            block
+            class="px-6 my-4 white--text"
+            color="#434b63"
+            @click="logOut"
+            >
+            LOG OUT
+            </v-btn>
+
+        </div>
     </div>
 </template>
+
+<script>
+export default({
+    name:"Home",
+    async created() {
+        if (!this.$store.getters.isLoggedIn) {
+            this.$router.push('/');
+        }
+    },
+    methods: {
+        logOut(){
+            this.$store.dispatch('logout');
+            this.$router.push('/');
+        }
+    
+    }
+    
+})
+</script>
