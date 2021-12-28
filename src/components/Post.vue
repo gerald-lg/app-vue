df4f22866eef185d3ce1b8036df45112adeab218
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-12-27 23:01:28 -0300

    feat(components/Post): se agrega middleware y refactoriza la vista

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -11,18 +11,18 @@
         <div class="d-flex flex-column my-2">
             <v-card 
             flat
-            class="text-uppercase overflow-hidden text-justify-center"
-            height="70"
+            class="text-uppercase overflow-hidden text-center text-truncate"
+            max-height="70"
             style="text-decoration: underline #847ae1; 
-            font-size: 20px"> {{title}}</v-card>
+            font-size: 20px"> {{this.title}}</v-card>
             <br>
             <v-card flat 
             width="250px" 
             height="125" 
-            class="overflow-hidden text-justify mx-auto">{{body}}</v-card>
+            class="overflow-hidden text-justify mx-auto text-nowrap">{{this.body}}</v-card>
         </div>
         <!-- Boton ver -->
-        <v-btn class="white--text font-weight-bold" width="250px" color="#d391af">VER</v-btn>
+        <v-btn @click="getPost(id)" class="white--text font-weight-bold" width="250px" color="#d391af">VER</v-btn>
 
     </div>
 </template>
@@ -38,7 +38,14 @@ export default ({
         body: String,
         status: Boolean,
         amount: Number
-    } 
+    }, methods: {
+        getPost(id){
+            this.$router.push({
+                path: "post/:id",
+                query: {id}
+            })
+        }
+    }
     
 })
 </script>
